34feedfbdb90c5f181ff01c71b1db82e91df0493
Luís Cláudio <luisclaudiogouveiarocha@gmail.com>
2018-01-10 14:16:14 -0300

    dummy integrator so to avoid segfaults

---
+++ include/integrator/pathtracer.h
@@ -0,0 +1,15 @@
+#ifndef PATHTRACER_H
+#define PATHTRACER_H
+
+#include "../core/integrator.h"
+
+class Pathtracer : public Integrator
+{
+private:
+  RGB integrate(const Vec2& uv, const Scene& scene) const override
+  {
+    return RGB(1.0f);
+  }
+};
+
+#endif
--- src/core/integrator.cpp
+++ src/core/integrator.cpp
@@ -1,4 +1,5 @@
 #include "../../include/core/integrator.h"
+#include "../../include/integrator/pathtracer.h"
 
 void Integrator::render(const Scene& scene)
 {
@@ -7,5 +8,5 @@ void Integrator::render(const Scene& scene)
 
 Integrator::ptr Integrator::load_from_json(const nlohmann::json& in)
 {
-
+  return Integrator::ptr( new Pathtracer );
 }
