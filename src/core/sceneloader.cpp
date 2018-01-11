a8c2c88dd2a8af696a8c83099758f50caa9f0d98
Luís Cláudio <luisclaudiogouveiarocha@gmail.com>
2018-01-11 18:07:06 -0300

    + generate_scene()

--- src/core/sceneloader.cpp
+++ src/core/sceneloader.cpp
@@ -30,27 +30,16 @@ void SceneLoader::generate_scene(Scene& target)
   //(or maybe not, as shared_ptr's will do this automatically)
   //and maybe apply some operation to all materials/textures
   //at once
-  std::vector<std::vector<Primitive::ptr>> unpacked_prims;
-  unpacked_prims.resize(shapes.size());
 
-  //generate primitives
-  int s_id = 0;
+  //generate primitives and store all materials and textures
+  //into target scene
   for( auto s : shapes )
   {
-    s->generate_primitives( unpacked_prims[s_id++] );
-    printf("generated %d primitives\n", unpacked_prims[s_id-1].size());
+    s->generate_primitives( target.prims );
+    for( auto m : s->materials ) target.materials.push_back(m);
+    for( auto t : s->textures ) target.textures.push_back(t);
   }
 
-  //pack textures and materials into a single vector
-  //this isn't really needed for the operations, but will
-  //help in managing
-  int tex_offset = 0;
-  for( auto s : shapes )
-  {
-    std::vector<Texture::ptr>& texs = s->textures;
-
-    //TODO: this is really inefficient!
-    for( auto t : texs ) target.textures.push_back(t);
-  }
-
+  target.cam = this->camera;
+  target.bgd = this->bgd;
 }
