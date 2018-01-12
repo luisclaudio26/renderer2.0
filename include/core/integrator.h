9d4d53511950f7b81000bec62e3f856c7ff586f1
Luís Cláudio <luisclaudiogouveiarocha@gmail.com>
2018-01-12 15:16:07 -0300

    use 8 threads to render. apparently some problem with normals!

--- include/core/integrator.h
+++ include/core/integrator.h
@@ -6,6 +6,7 @@
 #include "scene.h"
 #include "spectrum.h"
 #include "../../3rdparty/json.hpp"
+#include "../util/imagetools.h"
 
 class Integrator
 {
@@ -14,6 +15,8 @@ private:
   //luminance. this function is responsible for tracing primary rays!
   virtual RGB integrate(const Vec2& uv, const Scene& scene) const = 0;
 
+  void render_patch(ImageTools::RGBuchar* img, const Scene& scene,
+                    int row, int col, int height, int width, int vRes, int hRes);
 public:
   typedef std::shared_ptr<Integrator> ptr;
 
--- src/core/integrator.cpp
+++ src/core/integrator.cpp
@@ -1,14 +1,18 @@
 #include "../../include/core/integrator.h"
 #include "../../include/integrator/pathtracer.h"
-#include "../../include/util/imagetools.h"
+#include <thread>
+#include <cstdio>
 
-void Integrator::render(const Scene& scene)
+void Integrator::render_patch(ImageTools::RGBuchar* img, const Scene& scene,
+                              int row, int col, int height, int width,
+                              int vRes, int hRes)
 {
-  int hRes = 32, vRes = 24;
-  ImageTools::RGBuchar *img = new ImageTools::RGBuchar[hRes*vRes];
-
-  for(int i = 0; i < vRes; ++i)
-    for(int j = 0; j < hRes; ++j)
+  //this function is meant to be executed by an specific thread
+  //which will render this block of WidthxHeight size, starting
+  //at (row, col)
+  //TODO: some bug is causing the image to be flipped, check this after!
+  for(int i = row; i < row+height; ++i)
+    for(int j = col; j < col+width; ++j)
     {
       float half_vRes = vRes*0.5f, half_hRes = hRes*0.5f;
       float u = (half_vRes - i)/ half_vRes;
@@ -17,6 +21,35 @@ void Integrator::render(const Scene& scene)
       RGB sample = integrate(Vec2(u,v), scene);
       img[i*hRes+j] = ImageTools::rgb_float_to_uchar(sample);
     }
+}
+
+void Integrator::render(const Scene& scene)
+{
+  int hRes = 160, vRes = 120;
+  ImageTools::RGBuchar *img = new ImageTools::RGBuchar[hRes*vRes];
+
+  //TODO: general code for spawning threads:
+  //-> find the least p,q such that p.q = N and divide in PxQ sectors
+  //adjustments are needed because this will fail if image dimension is
+  //not multiple of p and q!!!
+  std::vector<std::thread> rendering_threads;
+  int vert_sectors = 2, hori_sectors = 4;
+
+  for(int i = 0; i < vert_sectors; ++i)
+    for(int j = 0; j < hori_sectors; ++j)
+    {
+      int height = vRes / vert_sectors;
+      int width = hRes / hori_sectors;
+      int row = i * height;
+      int col = j * width;
+
+      rendering_threads.push_back(std::thread(&Integrator::render_patch, this,
+                                              img, scene, row, col,
+                                              height, width, vRes, hRes));
+    }
+
+  for(auto t = rendering_threads.begin(); t != rendering_threads.end(); ++t)
+    t->join();
 
   ImageTools::rgb_write_to_file("../output/out.ppm", hRes, vRes, img);
   delete[] img;
