e54d8ce7422043c1eb74368ddddf0c6ac5be9e0c
Luís Cláudio <luisclaudiogouveiarocha@gmail.com>
2018-01-13 19:49:05 -0300

    + tree building. not tested yet!

--- include/kdtree/kdtree.h
+++ include/kdtree/kdtree.h
@@ -8,16 +8,18 @@ class KdNode
 {
 public:
   AABB aabb;
-  int axis;
   KdNode *left, *right;
 
   //TODO: we're mixing Leaf and internal node
   //data. split this (union? inheritance?)
   std::vector<int> prims;
 
-  float split_at(const KdNode& n, int& axis);
+  float split_at(const std::vector<Primitive::ptr>& prims,
+                          const std::vector<int>& prims_ids, int& axis);
+
   KdNode() : left(NULL), right(NULL) {}
-  KdNode(const std::vector<int>& prims, const AABB& aabb);
+  KdNode(const std::vector<Primitive::ptr>& prims,
+          const std::vector<int>& prims_ids, const AABB& aabb);
 };
 
 class KdTree
--- src/kdtree/kdtree.cpp
+++ src/kdtree/kdtree.cpp
@@ -1,8 +1,11 @@
 #include "../../include/kdtree/kdtree.h"
 
-static void compute_aabb(const std::vector<Primitive::ptr>& prims)
+static const int MAX_PRIM = 50;
+
+static void compute_aabb(const std::vector<Primitive::ptr>& prims, AABB& target)
 {
-  Vec3 min(FLT_MAX); Vec3 max(-FLT_MAX);
+  target.min = Vec3(FLT_MAX);
+  target.max = Vec3(-FLT_MAX);
 
   for(auto p : prims)
   {
@@ -11,8 +14,8 @@ static void compute_aabb(const std::vector<Primitive::ptr>& prims)
       {
         //TODO: is it necessary to take the minimum of
         //p_aabb's min and max corners? I don't think so
-        min[i] = std::fmin(p_aabb.min[i], min[i]);
-        max[i] = std::fmax(p_aabb.max[i], max[i]);
+        target.min[i] = std::fmin(p_aabb.min[i], target.min[i]);
+        target.max[i] = std::fmax(p_aabb.max[i], target.max[i]);
       }
   }
 }
@@ -20,23 +23,71 @@ static void compute_aabb(const std::vector<Primitive::ptr>& prims)
 //-----------------------------------
 //---------- FROM KDTREE.H ----------
 //-----------------------------------
-float KdNode::split_at(const KdNode& n, int& axis)
+float KdNode::split_at(const std::vector<Primitive::ptr>& prims,
+                        const std::vector<int>& prims_ids, int& axis)
 {
+  //TODO: this is not a good way of selecting the axis!
+  static int axis_cnt = 0;
+  axis = (axis_cnt++) % 3;
 
+  //TODO: this is not a good heuristic for primitive splitting
+  float avg = 0.0f;
+  for(auto id : prims_ids)
+  {
+    AABB p_aabb; prims[id]->aabb(p_aabb);
+    avg += p_aabb.min[axis];
+  }
+
+  return avg / prims_ids.size();
 }
 
-KdNode::KdNode(const std::vector<int>& prims, const AABB& aabb)
+KdNode::KdNode(const std::vector<Primitive::ptr>& prims,
+                const std::vector<int>& prims_ids, const AABB& aabb)
 {
+  //stop criterion
+  if(prims_ids.size() < MAX_PRIM)
+  {
+    this->left = this->right = NULL;
+    this->prims = std::move( prims_ids );
+    this->aabb = aabb;
+  }
+  else
+  {
+    //choose splitting point
+    float split; int axis;
+    split = split_at(prims, prims_ids, axis);
 
+    //classify primitives into left/right
+    std::vector<int> left, right;
+    for(auto id : prims_ids)
+    {
+      //TODO: precompute bounding boxes
+      Primitive::ptr p = prims[id];
+      AABB p_aabb; p->aabb(p_aabb);
+
+      if(p_aabb.min[axis] < split) left.push_back( id );
+      if(p_aabb.max[axis] > split) right.push_back( id );
+    }
+
+    //compute new bounding boxes
+    AABB left_aabb = aabb; left_aabb.max[axis] = split;
+    AABB right_aabb = aabb; right_aabb.min[axis] = split;
+
+    //recursively build children
+    this->left = new KdNode(prims, left, left_aabb);
+    this->right = new KdNode(prims, right, right_aabb);
+  }
 }
 
 void KdTree::build(const std::vector<Primitive::ptr>& prims)
 {
   //this vector will contain indices to all primitives
   //we want to store inside the tree
-  std::vector<int> all_prims;
+  std::vector<int> prims_ids;
   for(int i = 0; i < prims.size(); ++i)
-    all_prims.push_back(i);
+    prims_ids.push_back(i);
 
-  root = KdNode();
+  AABB full_aabb; compute_aabb(prims, full_aabb);
+
+  root = KdNode(prims, prims_ids, full_aabb);
 }
